388ac1179ae33184a5ce5d1d3845d3d98d6c760d
ystsai <ystsai7981@gmail.com>
2021-12-14 15:30:27 +0800

    多回傳一個player(chessman)

--- Player.py
+++ Player.py
@@ -4,14 +4,63 @@ import random
 
 class player:
     def __init__(self):
-        self.tree = tree()
-        pass
+        self.board = go.BuildBoard(8)
+        self.valid_moves = [[i, j] for i in range(8) for j in range(8)]
+        self.root = node()
+        self.current_node = self.root
     
-    def move(self, board, laststep):
-        return random.choice(go.GetValid(board))
+    def move(self, board, last_step, player):
+        if not last_step:
+            self.root.player = -player
+            self.current_node = self.root
+            next_step = random.choice(self.valid_moves)
+            self.root_forward(next_step)
+            return self.root.move
+        else:
+            self.root_forward(last_step)
+            self.current_node = self.root
+            next_step = random.choice(self.valid_moves)
+            self.root_forward(next_step)
+            return self.root.move
+        
+    def root_forward(self, next_step):
+        if not self.root.children:
+            self.root.expand(self.valid_moves)
+        next_index = self.valid_moves.index(next_step)
+        self.root = self.root.children[next_index]
+        self.root.parrent = None
+        self.board[next_step] = self.root.player
+        self.valid_moves.remove(next_step)
+
+    def forward(self, board, valid_moves, next_step, player):
+        row, col = next_step
+        assert board[row, col] == 0, "This position has another disc."
+        board[row, col] = player
+        assert next_step in valid_moves, "Next step is not in valid moves."
+        valid_moves.remove(next_step)
+        
+    def selection(self, board, valid_moves):
+        if self.current_node.isLeaf():
+            return
+        else:
+            next_node = self.current_node.find_max_UCB_child()
+            self.current_node = next_node
+            self.forward(board, valid_moves, next_node.move, next_node.player)
+            self.selection(board, valid_moves)
+            
+    # def expansion(self):
+    #     self.current_node.expand(self.valid_moves_cp)
+    #     if self.current_node.children:
+    #         next_node = self.current_node.pick()
+    #         self.action(self.board_cp, next_node.player, next_node.get_move(), self.valid_loc_cp, self.valid_moves_cp)
+    #         self.current_node = next_node
+    #         self.current_player = -next_node.player
+
+    
+    
     
 class node:
-    def __init__(self, player=None, move=None, parent=None):
+    def __init__(self, player=1, move=None, parent=None):
         self.player = player
         self.move = move
         self.Ni = 0
@@ -34,6 +83,7 @@ class node:
             return self.Wi/self.Ni
         
     def find_max_UCB_child(self):
+        assert self.children, "No child to find max UCB."
         idx = 0
         max_UCB = -float("inf")
         for i, child in enumerate(self.children):
@@ -43,9 +93,38 @@ class node:
                 max_UCB = UCB
         return self.children[idx]
     
-class tree:
-    def __init__(self):
-        self.board = go.BuildBoard(8)
-        self.root = node()
+    def find_max_score_child(self):
+        assert self.children, "No child to find max score."
+        idx = 0
+        max_score = -float("inf")
+        for i, child in enumerate(self.children):
+            score = child.score()
+            if score > max_score:
+                idx = i
+                max_score = score
+        return self.children[idx]
+    
+    def isLeaf(self):
+        if not self.children:
+            return True
+        else:
+            return False
+        
+    def inTree(self):
+        if self.Ni != 0:
+            return True
+        else:
+            return False
+        
+    def expand(self, valid_moves):
+        assert valid_moves, "No valid moves to expand."
+        for i in valid_moves:
+            self.children.append(node(-self.player, i, self))
+                
+    def update(self, winner):
+        self.Ni += 1
+        if winner == self.player:
+            self.Wi += 1
+    
         
     
